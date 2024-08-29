58fe619ab3bd1dc384af0690068e234817af5b3c
Beniamino-Soccio <beniamino43@gmail.com>
2024-08-29 17:07:36 +0200

    snack-2 + end of exercise

--- js/script.js
+++ js/script.js
@@ -25,30 +25,6 @@ guests.forEach((guest, i) => {
 })
 console.log(segnaposti);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 *SNACK 2*
 Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
@@ -67,3 +43,23 @@ Id  Name                Grades
 102 Piero della Francesca   50
 120 Francesca da Polenta    84
 */
+//inizializzo l'array studenti
+const students = [
+    {id: 213, name: 'Marco della Rovere', grades: 78},
+    {id: 110, name:  'Paola Cortellessa', grades: 96},
+    {id: 25, name: 'Andrea Mantegna', grades: 48},
+    {id: 145, name: 'Gaia Borromini' , grades: 74},
+    {id: 196, name:  'Luigi Grimaldello', grades: 68},
+    {id: 102, name:  'Piero della Francesca', grades: 50},
+    {id: 120, name:  'Francesca da Polenta', grades: 84}
+]
+console.log(students);
+//array nome in maiuscolo
+const studentUpperCase = students.map(student => student.name.toUpperCase());
+console.log(studentUpperCase);
+//array studenti che hanno un totale di voti superiore a 70
+const gradesOver70 = students.filter(student => student.grades >= 70);
+console.log(gradesOver70);
+//array studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+const gradesOver70Id120 = students.filter(student => student.grades >= 70 && student.id > 120);
+console.log(gradesOver70Id120);
